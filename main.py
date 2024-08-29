28b9af08fa3fdb39ee9e280930036de2b25a7a9f
Tiffany <tgateley22@gmail.com>
2024-08-29 11:17:06 -0500

    add a store and read function to store extracted data in a txt file

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import requests
 import selectorlib
 
-
 URL = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) '
@@ -21,7 +20,26 @@ def extract(text):
     return value
 
 
+def send_email():
+    print("Email was sent")
+
+
+def store(extracted):
+    with open("data.txt", 'a') as file:
+        file.write(extracted + "\n")
+
+
+def read(extracted):
+    with open("data.txt", "r") as file:
+        return file.read()
+
+
 if __name__ == "__main__":
     scraped = scrape(URL)
-    print(extract(scraped))
-
+    extracted = extract(scraped)
+    print(extracted)
+    file = read(extracted)
+    if extracted != "No upcoming tours":
+        if extracted not in file:
+            store(extracted)
+            send_email()
